0060e3e97178fca6c915ab153c58fd586e88be42
reyuki <hikikomori.yukisan@gmail.com>
2024-03-29 13:52:12 +0700

    fix leak bot token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const path = require("node:path")
 const { Client, SessionManager } = require('gampang');
+require("dotenv").config()
 
 const session = new SessionManager(
     path.resolve(__dirname, 'sessions'),
@@ -26,11 +27,11 @@ client.on('ready', () => {
 client.command('stele', async (ctx) => {
     const args = ctx.text.split(" ");
     try {
-        const files = await fetch(`https://api.telegram.org/bot6256334764:AAFvw3YGLYVJSYho3Gu-VZH_jAyhddXyLpk/getStickerSet?name=${args[1]}`).then(res => res.json());
+        const files = await fetch(`https://api.telegram.org/bot${process.env.TELE_TOKEN}/getStickerSet?name=${args[1]}`).then(res => res.json());
         if (files.ok) {
             files.result.stickers.forEach(async (file) => {
-                const st = await fetch(`https://api.telegram.org/bot6256334764:AAFvw3YGLYVJSYho3Gu-VZH_jAyhddXyLpk/getFile?file_id=${file.file_id}`).then(res => res.json()).catch(console.log);
-                const sticker = await fetch(`https://api.telegram.org/file/bot6256334764:AAFvw3YGLYVJSYho3Gu-VZH_jAyhddXyLpk/${st.result.file_path}`).catch(console.log);
+                const st = await fetch(`https://api.telegram.org/bot${process.env.TELE_TOKEN}/getFile?file_id=${file.file_id}`).then(res => res.json()).catch(console.log);
+                const sticker = await fetch(`https://api.telegram.org/file/bot${process.env.TELE_TOKEN}/${st.result.file_path}`).catch(console.log);
                 await ctx.replyWithSticker(sticker).catch(console.log);
             })
         }
