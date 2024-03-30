7ef3cdb49bcd3f191f1f51a1678dfadc156b75a0
RealYukiSan <hikikomori.yukisan@gmail.com>
2024-03-30 19:14:04 +0700

    update menu and stele

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ client.command('tostkr', async (ctx) => {
 });
 
 client.command('menu', (ctx) => {
-    const commands = ['tostkr', 'toimg', 'ping', 'stele'].map(cmd => ctx.getPrefix()+cmd)
+    const commands = ['tostkr <reply gambar>', 'toimg <reply sticker>', 'ping', 'stele bokepanak1'].map(cmd => ctx.getPrefix()+cmd)
     const text = `Current available commands:\n${commands.join('\n')}`
     ctx.reply(text);
 }, { aliases: ['help']})
@@ -53,17 +53,24 @@ client.command('menu', (ctx) => {
 client.command('stele', async (ctx) => {
     const args = ctx.text.split(" ");
     try {
+        if (args[1].startsWith("https://t.me/addstickers/")) args[1] = args[1].slice(25)
         const files = await fetch(`https://api.telegram.org/bot${process.env.TELE_TOKEN}/getStickerSet?name=${args[1]}`).then(res => res.json());
         if (files.ok) {
+            await ctx.reply("CPM kak.")
+            ctx.raw.key.remoteJid = ctx.raw.key.participant
             files.result.stickers.forEach(async (file) => {
                 const st = await fetch(`https://api.telegram.org/bot${process.env.TELE_TOKEN}/getFile?file_id=${file.file_id}`).then(res => res.json()).catch(console.log);
                 const sticker = await fetch(`https://api.telegram.org/file/bot${process.env.TELE_TOKEN}/${st.result.file_path}`).catch(console.log);
                 await ctx.replyWithSticker(sticker).catch(console.log);
             })
-        }
+        } else ctx.reply("invalid sticker name or url")
     } catch (error) {
         console.log(error);
     }
+})
+
+client.command('tagall', (ctx) => {
+    ctx.client.raw?.sendMessage(ctx.raw.key.remoteJid, { text: "coming soon" }, { quoted: ctx.raw })
 })
 
 client.launch();
